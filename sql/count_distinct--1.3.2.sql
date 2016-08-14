f6b1bdf79b5ddeec0ae2dd1626197c626f96f510
Tomas Vondra <tv@fuzzy.cz>
2016-08-14 20:28:30 +0200

    add array_agg_distinct(...) as an alternative to array_agg(distinct ...)

--- sql/count_distinct--1.3.2.sql
+++ sql/count_distinct--1.3.2.sql
@@ -15,3 +15,15 @@ CREATE AGGREGATE count_distinct(anyelement) (
     STYPE = internal,
     FINALFUNC = count_distinct
 );
+
+CREATE OR REPLACE FUNCTION array_agg_distinct(internal, anynonarray)
+    RETURNS anyarray
+    AS 'count_distinct', 'array_agg_distinct'
+    LANGUAGE C IMMUTABLE;
+
+CREATE AGGREGATE array_agg_distinct(anynonarray) (
+    SFUNC = count_distinct_append,
+    STYPE = internal,
+    FINALFUNC = array_agg_distinct,
+    FINALFUNC_EXTRA
+);
